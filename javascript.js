ceb426091fb104fd42fd0778d5bbe4a36a14322f
florenciapaula <89614423+florenciapaula@users.noreply.github.com>
2021-10-02 20:10:11 -0300

    Add files via upload
    update

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
 
  function validacion(mail) {
 
+
+
 	var nombre = document.getElementById('nombre').value;
 	var mail = document.getElementById('mail').value;
 	var tel = document.getElementById('tel').value;
 	var subject = document.getElementById('subject').value;
 	var comentarios = document.getElementById('comentarios').value;
 
+
 	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+
+	if(nombre != "" && mail != "" && tel != "" && subject != "" && comentarios!= ""){
 
 	if(!re.exec(mail)){
 
 	  alert('ingrese un mail valido');
+      return (false);
 	  
 	}
-
+	
 	else{
+      
+	  alert('Se abrira una ventana para enviar tu mensaje');
+	  window.location.href ='mailto:mydonut@gmail.com?subject='
+                         + encodeURIComponent(subject)
+                         + "&body=" 
+                         + encodeURIComponent(
+					     "Nombre y Apellido: " + nombre + "\r" +
+						 "Mail: " + mail + "\r" +
+						 "Tel: " + tel + "\r" +
+						 "Comentarios: " + comentarios);
 
-	  alert('Mensaje enviado');
-	 // window.open("https://mail.google.com/mail/u/0/?tab=rm#inbox?compose=new", "_blank");
-  }
-}
+	}
+}}
